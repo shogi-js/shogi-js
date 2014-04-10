bfc4137fb69c4e2ce1616e399307eba691c5ee3b
bgnori <bgnori@gmail.com>
2014-04-10 18:08:54 +0900

    position setup with csa format

--- game.js
+++ game.js
@@ -4,6 +4,14 @@ $(document).ready(function() {
 
     var g_squares = {};
 
+    function index2coor_x(idx) {
+        return (9 - idx) * 60 + 30;
+    };
+
+    function index2coor_y(idx) {
+        return (idx - 1) * 64 + 32;
+    };
+
     Crafty.init(600, 640);
     Crafty.canvas.init();
 
@@ -117,6 +125,53 @@ $(document).ready(function() {
         },
     }); //Crafty.c Piece
 
+    function initial_setup() {
+        csa_setup(
+            "P1-KY-KE-GI-KI-OU-KI-GI-KE-KY",
+            "P2 * -HI *  *  *  *  * -KA * ",
+            "P3-FU-FU-FU-FU-FU-FU-FU-FU-FU",
+            "P4 *  *  *  *  *  *  *  *  * ",
+            "P5 *  *  *  *  *  *  *  *  * ",
+            "P6 *  *  *  *  *  *  *  *  * ",
+            "P7+FU+FU+FU+FU+FU+FU+FU+FU+FU",
+            "P8 * +KA *  *  *  *  * +HI * ",
+            "P9+KY+KE+GI+KI+OU+KI+GI+KE+KY");
+    };
+    
+    function csa_setup(argv) {
+        var rank = new RegExp("^P(\\d)");
+        var sq = new RegExp("((?:[-+])(?:FU|KY|KE|GI|KI|KA|HI|OU|TO|NY|NK|NG|UM|RY))|( \\* )", 'g');
+
+        var pieces = [];
+        
+        for (var i in csa_setup.arguments) {
+            var s = csa_setup.arguments[i];
+            //console.log(s);
+            var xs = s.split(rank);
+            //console.log(xs);
+            var r = parseInt(xs[1]);
+            console.log("i+1 and r", parseInt(i) + 1, r);
+            var x = 1;
+            _.each(xs[2].match(sq), function(elem, index, xs) {
+                if (elem[0] == '-' || elem[0] == '+') {
+                    var pn;
+                    if (elem[0] == '-') {
+                        pn = elem.substring(1,3).toLowerCase();
+                    } else {
+                        pn = elem.substring(1,3);
+                    }
+                    console.log('entity for', 9 - index, r, pn);
+                    var piece = Crafty.e("2D, DOM, Mouse, Draggable, Collision, " + pn + ", Piece");
+                    piece.attr({x:index2coor_x(9 - index), y:index2coor_y(r)});
+                    pieces.push(piece);
+
+                }
+                //console.log(x, index + 1);
+                x += 1;
+            })
+        };
+        return pieces;
+    };
 
     Crafty.scene("main", function() {
         Crafty.background("#FFFFFF url(assets/board.jpg) no-repeat center center");
@@ -152,19 +207,21 @@ $(document).ready(function() {
 
         _.each(_.range(1, 10, 1), function (x) {
             _.each(_.range(1, 10, 1), function (y) {
-                console.log(x, y);
+                //console.log(x, y);
                 var entity = Crafty.e("2D, DOM, Text, Region, Collision")
                     .text("" + x + ","+ y)
                     .attr({
                         visible:false,
-                        x: (9 - x) * 60 + 30,
-                        y: (y - 1) * 64 + 32,
+                        x: index2coor_x(x),
+                        y: index2coor_y(y),
                         w: 60,
                         h: 64,});
                 entity.attr({idx_x:x, idx_y:y});
                 g_squares[(x, y)] = entity;
             });
         });
+        initial_setup();
+        /*
         var x = 1;
         var y = 1;
         for(var i in PIECE_NAMES ){
@@ -177,5 +234,6 @@ $(document).ready(function() {
                 x = 0
             }
         };
+        */
     });
 })
