7e1e46d0951612a956b91194c83d1a228149dee3
bgnori <bgnori@gmail.com>
2014-04-18 21:27:48 +0900

    migrated to yeoman, bower, grunt enviroment!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,10 @@
+require('scripts/bower_components/underscore/underscore');
+require('scripts/bower_components/jquery/jquery');
+require('scripts/bower_components/crafty/crafty');
 require('scripts/components/*');
 require('scripts/scenes/*');
-
+require('scripts/shogi');
 require('scripts/game');
 
-Crafty.debugBar.show();
-
+//Crafty.debugBar.show();
 
--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,18 +1,93 @@
 'use strict';
 
-var Game = {
-  // Initialize and start our game
+var TestBed = {
   start: function () {
-    Crafty.load([ ], function () {
 
-      // Start crafty and set a background color so that we can see it's working
-      Crafty.init(1000, 1000);
-      Crafty.background('#FFFFFF');
+    Crafty.init(200 + 600 + 200, 1000);
+    Crafty.canvas.init();
 
-      // Simply start splashscreen
-      //Crafty.scene('Splashscreen');
+    Crafty.scene("loading", function() {
+        // Load our assets
+        Crafty.load([ "sounds/opening2.mp3", "sounds/snap.mp3",
+                "sounds/ncoin.mp3",
+                //"sounds/opening2.ogg",
+                //"sounds/snap.ogg",
+                //"sounds/ncoin.ogg",
+                "sounds/opening2.wav",
+                "sounds/snap.wav",
+                "sounds/ncoin.wav",
+                "images/koma.png",
+                "images/board.jpg",
+            ], 
+            function() {
+                Crafty.scene("main");
+            },
+            function() {
+                // progress
+            },
+            function(what) {
+                Crafty.e("2D, DOM, Text")
+                    .attr({w: 1000, h: 20, x: 0, y: 120})
+                    .text("error!:"+ what)
+                    .css({"text-align": "center", "font-size": "30px"});
+            }
+        );
+
+        // White background and some ugle text
+        Crafty.background("#FFF");
+        Crafty.e("2D, DOM, Text")
+            .attr({w: 1000, h: 20, x: 0, y: 120})
+            .text("Loading")
+            .css({"text-align": "center", "font-size": "30px"});
+    }); //scene loading
+
+
+    Crafty.scene("main", function() {
+        Crafty.background("#FFFFFF url(images/tatami.jpg) repeat");
+        Crafty.sprite("images/board.jpg", {SpriteBoard: [0, 0, 600, 640]});
+        Crafty.sprite("images/mokume.png", {SpriteKomadai: [0, 0, 160, 380]});
+        Crafty.sprite("images/koma.png", {SpritePiece:[0,0,60,64]});
+        
+        _.each(["mp3", "wav", "ogg"], function(ext){
+            console.log("audio:", ext, Crafty.audio.supports(ext));
+        });
+        Crafty.audio.add({
+            welcome: [
+                "sounds/opening2.mp3", 
+                //"sounds/opening2.ogg",
+                "sounds/opening2.wav"],
+            snap: [
+                "sounds/snap.mp3",
+                //"sounds/snap.ogg",
+                "sounds/snap.wav"],
+            coin: [
+                "sounds/ncoin.mp3",
+                //"sounds/ncoin.ogg",
+                "sounds/ncoin.wav"]});
+        Crafty.audio.play("welcome");
+
+
+
+        var game = Crafty.e("Shogi");
+        var board = Crafty.e("2D, Dom, Board, SpriteBoard");
+        board.game = game;
+        board.attr({x: 200, y: 0, off_x:30, off_y:32, z:50, cell_w: 60, cell_h: 64});
+        game.komaDai = {};
+        game.komaDai['+'] = Crafty.e("2D, Dom, SpriteKomadai, Region, Collision, Stack");
+        game.komaDai['+'].attr({x:820, y:240, z:51, name:"+"})
+        game.komaDai['-'] = Crafty.e("2D, Dom, SpriteKomadai, Region, Collision, Stack");
+        game.komaDai['-'].attr({x:20, y:20, z:51, name:"-"})
+        board.layout();
+        board.initial_setup();
+
+        var recorder = Crafty.e("2D, DOM, Text, Recorder");
+        recorder.attr({x:0, y:700, w:800, h:200, background_color: "white"});
+        recorder.text("test! test! test!");
+        recorder.startListen(game);
     });
-  }
+
+    Crafty.scene("loading");
+  },
 };
 
-Game.start();
+TestBed.start();
